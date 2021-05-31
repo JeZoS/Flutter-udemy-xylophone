08ff7e805fed241bef076d86d3cfc8f37ec4676a
JeZoS <prabhat262111@gmail.com>
2021-05-31 22:04:58 +0530

    Connected buttons and audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
+//
+//
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+  void playSound(int number) {
+    player.play('assets_note$number.wav');
+  }
+
+  Widget buildKey(int number, Color color) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(number);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+        ),
+        child: Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.pink.shade200),
+              buildKey(3, Colors.green.shade200),
+              buildKey(4, Colors.blue),
+              buildKey(5, Colors.orange),
+              buildKey(6, Colors.brown),
+              buildKey(7, Colors.black38),
+            ],
+          ),
         ),
       ),
     );
